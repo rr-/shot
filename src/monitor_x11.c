b447e59f00d7ce9b98c89bcbae942da4e011bcaa
rr- <mkurczew@gmail.com>
2015-07-17 20:50:01 +0200

    Fixed monitor management on X11/XRandR

--- src/monitor_x11.c
+++ src/monitor_x11.c
@@ -5,9 +5,25 @@
 
 unsigned int monitor_count()
 {
+    int ret = 0;
     Display *display = XOpenDisplay(NULL);
     assert(display);
-    unsigned int ret = XScreenCount(display);
+
+    XRRScreenResources *screen = XRRGetScreenResources(
+        display, DefaultRootWindow(display));
+    assert(screen);
+
+    for (int i = 0; i < screen->noutput; i++)
+    {
+        XRROutputInfo *output_info = XRRGetOutputInfo(
+            display, screen, screen->outputs[i]);
+        assert(output_info);
+        if (output_info->connection == RR_Connected)
+            ++ret;
+        XRRFreeOutputInfo(output_info);
+    }
+
+    XRRFreeScreenResources(screen);
     XCloseDisplay(display);
     return ret;
 }
@@ -17,17 +33,41 @@ Monitor *monitor_get_impl(unsigned int n)
     Display *display = XOpenDisplay(NULL);
     assert(display);
 
-    XRRScreenResources *screen =
-        XRRGetScreenResources(display, DefaultRootWindow(display));
-    XRRCrtcInfo *crtc_info = XRRGetCrtcInfo(display, screen, screen->crtcs[0]);
+    XRRScreenResources *screen = XRRGetScreenResources(
+        display, DefaultRootWindow(display));
+    assert(screen);
 
-    Monitor *ret = monitor_create(
+    unsigned int count = 0;
+    XRROutputInfo *output_info = NULL;
+    for (int i = 0; i < screen->noutput; i++)
+    {
+        output_info = XRRGetOutputInfo(display, screen, screen->outputs[i]);
+        assert(output_info);
+        if (output_info->connection == RR_Connected)
+        {
+            if (count == n)
+                break;
+            count++;
+        }
+        XRRFreeOutputInfo(output_info);
+        output_info = NULL;
+    }
+
+    assert(output_info);
+
+    XRRCrtcInfo *crtc_info = XRRGetCrtcInfo(display, screen, output_info->crtc);
+    assert(crtc_info);
+
+    Monitor *monitor = monitor_create(
         crtc_info->x,
         crtc_info->y,
         crtc_info->width,
         crtc_info->height);
-    assert(ret);
+    assert(monitor);
 
+    XRRFreeCrtcInfo(crtc_info);
+    XRRFreeOutputInfo(output_info);
+    XRRFreeScreenResources(screen);
     XCloseDisplay(display);
-    return ret;
+    return monitor;
 }
